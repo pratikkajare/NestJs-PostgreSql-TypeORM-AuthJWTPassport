da62d25d40038b1439d6e9a434c202b3a11e0626
PratikKajare <pratikkajare826@gmail.com>
2022-07-19 18:13:37 +0530

    byid,delete,update,searchByStatus crud

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,57 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { TASK } from './task.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TASK, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get() //method (Get,Post,Delete)
-  getAllTAsks(): TASK[] {
-    return this.tasksService.getAllTasks();
+  // @Get() //method (Get,Post,Delete)
+  // getAllTAsks(): TASK[] {
+  //   return this.tasksService.getAllTasks();
+  // }  //NOt working with search
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): TASK[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): TASK {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
-
   //Body() is from thunder or postman to take input in json, form-encoded
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
+  createTask(@Body() createTaskDto: CreateTaskDto): TASK {
+    return this.tasksService.createTask(createTaskDto); //saved in TASK by getAllTAsks
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
   ): TASK {
-    return this.tasksService.createTask(title, description); //saved in TASK by getAllTAsks
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { TASK, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 // main idea of a provider is that it can be injected as a dependency;
@@ -13,8 +15,31 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTasksWithFilters(filterDto: GetTasksFilterDto): TASK[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    console.log(tasks);
+
+    return tasks;
+  }
+
+  getTaskById(id: string): TASK {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   //want to define in controllers
-  createTask(title: string, description: string) {
+  createTask(createTaskDto: CreateTaskDto) {
+    const { title, description } = createTaskDto;
     const task: TASK = {
       id: uuid(),
       title,
@@ -22,6 +47,14 @@ export class TasksService {
       status: TaskStatus.OPEN,
     };
     this.tasks.push(task);
+    return task;
+  }
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  updateTaskStatus(id: string, status: TaskStatus): TASK {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
